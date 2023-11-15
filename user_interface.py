43bf1109133b0fdde328d53f6ace66b96a2898eb
Eric Sakkas <esakkas@wesleyan.edu>
2023-11-15 10:27:58 -0500

    initialized user_interface with open_ui()

---
+++ user_interface.py
@@ -0,0 +1,9 @@
+def open_ui() -> None:
+    '''Opens an environment where user can input commands to the command line
+
+    Opens in the command line when user inputs the command 'stacker'
+    '''
+    pass
+
+
+
