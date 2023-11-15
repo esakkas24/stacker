52d638c009c4f4c7e3c7b81c1dc68d3cc5f66d31
Eric Sakkas <esakkas@wesleyan.edu>
2023-11-15 10:35:02 -0500

    convert_to_python skel. code. translates ui input

--- user_interface.py
+++ user_interface.py
@@ -15,8 +15,27 @@ def parse_command(command : str) -> dict:
     Returns:
         parsed_input (dict) : dictionary of input and flags
     
-    input: saveAmberParm -r 100 -s chr2 --outfile test.pdb input.pdb
-    parsed_input: {command : save_AmberParm, r : 100, s : chr2, outfile : test.pdb, input : input.pdb}
+    Example:
+        input: saveAmberParm -r 100 -s chr2 --outfile test.pdb input.pdb
+        parsed_input: {command : save_AmberParm, r : 100, s : chr2, outfile : test.pdb, input : input.pdb}
     '''
     pass
+
+def convert_to_python_command(parsed_input : dict) -> tuple:
+    '''Converts a parsed input to a python function from the module
+
+    Takes parsed command line input and converts it to the recognized function
+        from the module with the appropriate inputs.
+
+    Args:
+        parsed_input (dict) : dictionary of input and flags. Output of parse_command()
+    Returns:
+        function_and_agrs (tuple) : tuple of (function, args)
+            function (Python Function) : function to be run
+            args (dict) : dictionary where key is the function argument name and value is the value to be inputted
+
+    Example:
+        input: {command : save_AmberParm, r : 100, s : chr2, outfile : test.pdb, input : input.pdb}
+        ouput: (save_amber_parm, {region : 100, section : chr2, outfile : test.pdb, input : input.pdb})
+    '''
 
