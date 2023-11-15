b5748eb648333120c931717a85d5d0c34f667c93
Eric Sakkas <esakkas@wesleyan.edu>
2023-11-15 10:06:17 -0500

    heatmap visualization of two residue skel. code

--- visualization.py
+++ visualization.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import residue_movement
 
 def visualize_two_residue_movement_scatterplot(csv_filepath : str) -> None:
-    '''Creates scatteprlot of two-residue movement relative to each other
+    '''Creates scatterplot of two-residue movement relative to each other
 
     Args:
         csv_filepath (str) : filepath to csv file containing data on the movement
@@ -15,3 +15,20 @@ def visualize_two_residue_movement_scatterplot(csv_filepath : str) -> None:
         scatterplot similar to the Figure D link in Proposal Feature 4.
     '''
     pass
+
+def visualize_two_residue_movement_heatmap(csv_filepath : str) -> None:
+    '''Creates heatmap of two-residue movement relative to each other
+
+    Args:
+        csv_filepath (str) : filepath to csv file containing data on the movement
+            of two residues relative to each other (r, rho, and theta values). Created
+            in residue_movement
+    Returns:
+        None
+    
+    Should run visualize_two_residue_movement_scatterplot() to set the current pyplot as
+        a scatterplot of the data and then adjust this scatterplot to be a heatmap instead.
+        Should now look identical to Figure D link in the proposal.
+    '''
+    pass
+
