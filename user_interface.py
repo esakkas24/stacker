4202eaa3c7ac2a4446ceb5cef27ea426cbf07735
Eric Sakkas <esakkas@wesleyan.edu>
2023-11-15 10:28:26 -0500

    Added parse_command() to interpret user input

--- user_interface.py
+++ user_interface.py
@@ -5,5 +5,18 @@ def open_ui() -> None:
     '''
     pass
 
+def parse_command(command : str) -> dict:
+    '''Parses a user's input to the user interface
 
+    Separates the user's command into the function, optional flags, and input
+
+    Args:
+        command (str) : command inputted to the UI
+    Returns:
+        parsed_input (dict) : dictionary of input and flags
+    
+    input: saveAmberParm -r 100 -s chr2 --outfile test.pdb input.pdb
+    parsed_input: {command : save_AmberParm, r : 100, s : chr2, outfile : test.pdb, input : input.pdb}
+    '''
+    pass
 
