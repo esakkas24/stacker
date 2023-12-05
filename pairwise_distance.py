d984a3d2ce83ada70a88dda0dd30a6bd3472c2fd
Eric Sakkas <esakkas@wesleyan.edu>
2023-12-05 13:25:18 -0500

    Res center calc. in calculate_res_distance()

--- pairwise_distance.py
+++ pairwise_distance.py
@@ -1,6 +1,8 @@
 import mdtraj as md
 import pandas
 import numpy as np
+import residue_movement
+from vector import *
 
 def calculate_residue_distance(trajectory : md.Trajectory, 
                                res1_num : int, res2_num : int, 
@@ -27,8 +29,14 @@ def calculate_residue_distance(trajectory : md.Trajectory,
     res2_atom_idx_query = "(name " + res2_atoms[0] + " or name " + res2_atoms[1] + " or name " + res2_atoms[2] + ") and residue " + str(res2_num)
     res2_atom_idices = topology.select(res2_atom_idx_query)
 
-    
+    res1_atom_xyz = trajectory.xyz[0, res1_atom_idices,:]
+    vectorized_res1_atom_xyz = [Vector(x,y,z) for [x,y,z] in res1_atom_xyz]
+    res1_center_of_geometry = residue_movement.calc_center_3pts(*vectorized_res1_atom_xyz)
 
+    res2_atom_xyz = trajectory.xyz[0, res2_atom_idices,:]
+    vectorized_res2_atom_xyz = [Vector(x,y,z) for [x,y,z] in res2_atom_xyz]
+    res2_center_of_geometry = residue_movement.calc_center_3pts(*vectorized_res2_atom_xyz)
+    print(res1_center_of_geometry)
 
 def get_residue_distance_for_frame(trajectory : md.Trajectory, frame : int) -> np.array:
     '''Calculates pairwise the distance between all residues in a given frame
@@ -63,3 +71,6 @@ if __name__ == "__main__":
     viewed_atom2_name = "C4"
     viewed_atom3_name = "C6"
     ############################
+
+    trj = md.load('first10_5JUP_N2_tUAG_aCUA_+1GCU_nowat.mdcrd', top = '5JUP_N2_tUAG_aCUA_+1GCU_nowat.prmtop')
+    calculate_residue_distance(trj[0], 426,427)
