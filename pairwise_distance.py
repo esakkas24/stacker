72ecdd86a054be6952840b8d43592d6e4dabb1df
Eric Sakkas <esakkas@wesleyan.edu>
2023-12-06 11:20:15 -0500

    corrected mdtraj, mdcrd unit discripency

--- pairwise_distance.py
+++ pairwise_distance.py
@@ -35,17 +35,13 @@ def calculate_residue_distance(trajectory : md.Trajectory,
     res1_atom_idices = topology.select(res1_atom_idx_query)
     res2_atom_idx_query = "(name " + res2_atoms[0] + " or name " + res2_atoms[1] + " or name " + res2_atoms[2] + ") and residue " + str(res2_num)
     res2_atom_idices = topology.select(res2_atom_idx_query)
-    
-    # THE PDB sees 426 as a C which it celarly isn't, let's save as full PDB and see hwere the issur is
 
-    table, bonds = trajectory.atom_slice(res1_atom_idices).topology.to_dataframe()
-    print(table.head())
-    res1_atom_xyz = trajectory.xyz[0, res1_atom_idices,:]
+    # convert nanometer units in trajectory.xyz to Angstroms
+    res1_atom_xyz = trajectory.xyz[0, res1_atom_idices,:] * 10
     vectorized_res1_atom_xyz = [Vector(x,y,z) for [x,y,z] in res1_atom_xyz]
-    for coords in vectorized_res1_atom_xyz: print(coords)
     res1_center_of_geometry = calc_center_3pts(*vectorized_res1_atom_xyz)
 
-    res2_atom_xyz = trajectory.xyz[0, res2_atom_idices,:]
+    res2_atom_xyz = trajectory.xyz[0, res2_atom_idices,:] * 10
     vectorized_res2_atom_xyz = [Vector(x,y,z) for [x,y,z] in res2_atom_xyz]
     res2_center_of_geometry = calc_center_3pts(*vectorized_res2_atom_xyz)
 
