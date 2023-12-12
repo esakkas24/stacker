142b96b7df84452223ae09252d8fb41e08320570
Eric Sakkas <esakkas@wesleyan.edu>
2023-12-12 13:46:22 -0500

    filter_traj unit tests

--- file_manipulation.py
+++ file_manipulation.py
@@ -15,10 +15,10 @@ def filter_traj(trajectory_filename : str, topology_filename : str,
             1 in 50 frames sampled trajectories for each replicate.
         topology_filename : str
             path to file of the topology of the molecule
-        atomnames_desired : set 
-            atomnames to keep in the trajectory
         residues_desired : set
             residue numbers of residues to keep in the trajectory
+        atomnames_desired : set 
+            atomnames to keep in the trajectory
 
     Returns:
         filtered_trajectory : md.Trajectory
@@ -33,7 +33,8 @@ def filter_traj(trajectory_filename : str, topology_filename : str,
     print("Filtering trajectory...")
     atomnames_query = " or ".join([f"name == '{atom}'" for atom in atomnames_desired])
     residues_query = " or ".join([f"residue == {resnum}" for resnum in residues_desired])
-    
+    print(atomnames_query,residues_query)
+
     if len(atomnames_query) == 0:
         if len(residues_query) == 0:
             filtered_trajectory = trajectory
@@ -46,13 +47,14 @@ def filter_traj(trajectory_filename : str, topology_filename : str,
             filtered_trajectory = trajectory.atom_slice(atom_indices_selection)
         else:
             atom_indices_selection = topology.select('(' + atomnames_query + ') and (' + residues_query + ')')
+            print(atom_indices_selection)
             filtered_trajectory = trajectory.atom_slice(atom_indices_selection)
     return filtered_trajectory
 
 def filter_traj_to_pdb(trajectory_filename : str, topology_filename : str, 
                        output_pdb_filename : str,
-                        atomnames_desired : set = {},
-                        residues_desired : set = {}) -> None:
+                        residues_desired : set = {},
+                        atomnames_desired : set = {}) -> None:
     '''Filters an input trajectory to only the specified atoms and residues and outputs to pdb
 
     Filteres an input trajectory that contains all of the atoms in a trajectory to only
@@ -68,10 +70,10 @@ def filter_traj_to_pdb(trajectory_filename : str, topology_filename : str,
             path to the output pdb file
         topology_filename : str
             path to file of the topology of the molecule
-        atomnames_desired : set 
-            atomnames to keep in the trajectory
         residues_desired : set
             residue numbers of residues to keep in the trajectory
+        atomnames_desired : set 
+            atomnames to keep in the trajectory
 
     Returns:
         None
@@ -79,22 +81,35 @@ def filter_traj_to_pdb(trajectory_filename : str, topology_filename : str,
     filtered_trajectory = filter_traj(trajectory_filename, topology_filename, residues_desired, atomnames_desired)
     filtered_trajectory.save_pdb(output_pdb_filename)
 
-def file_convert(input_file : str, ouput_filetype : str) -> None:
+def file_convert(trajectory_filename : str, topology_filename : str, output_file : str) -> None:
     '''Converts trajectory input file to new output type
     
+    Output filetype determined by output_file extension. Uses mdtraj.save() commands to convert 
+    trajectory files to various filetypes mdtraj.save_mdcrd(), mdtraj.save_pdb(), mdtraj.save_xyz(), etc
+
     Args:
-        input file (str) : path name of the input file to be converted
-        output_file (str) : desired filetype to convert to (mdcrd, pdb, etc.)
+        trajectory_filename : str
+            path to file of the concatenated trajectory. Should be resampled to the
+            1 in 50 frames sampled trajectories for each replicate.
+        topology_filename : str
+            path to file of the topology of the molecule
+        output_file : str
+            desired filetype to convert to (mdcrd, pdb, etc.)
     Returns:
         None
-
-    Uses mdtraj.save() commands to save to convert trajectories to various filetypes
-        mdtraj.save_mdcrd(), mdtraj.save_pdb(), mdtraj.save_xyz(), etc'''
-    pass
+    '''
+    trajectory = md.load(trajectory_filename, top = topology_filename)
+    trajectory.save(output_file)
 
 if __name__ == "__main__":
-    filtered_traj = filter_traj('first10_5JUP_N2_wCCC_+1GCU_nowat.mdcrd', '5JUP_N2_wCCC_+1GCU_nowat.prmtop', {'C2','C4','C6'}, {426,427})
-    print([residue for residue in filtered_traj.topology.residues])
-    print([atom for atom in filtered_traj.topology.atoms])
+    # filter_traj tests
+    print('Known Res: 425 = G and 426 = C')
+    filtered_traj = filter_traj('first10_5JUP_N2_tUAG_aCUA_+1GCU_nowat.mdcrd', '5JUP_N2_tUAG_aCUA_+1GCU_nowat.prmtop', {425,426}, {'C2','C4','C6'})
+    table, bonds = filtered_traj.topology.to_dataframe()
+    print(table)
+
+    ### No Filtering
+    print("No Filtering, known trj has 12089 atoms")
+    filtered_traj = filter_traj('first10_5JUP_N2_tUAG_aCUA_+1GCU_nowat.mdcrd', '5JUP_N2_tUAG_aCUA_+1GCU_nowat.prmtop', residues_desired={}, atomnames_desired={})
     table, bonds = filtered_traj.topology.to_dataframe()
     print(table)
