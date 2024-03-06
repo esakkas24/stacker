c925c2c5b40b38945678af8e38b65f91a92e18fa
Eric Sakkas <esakkas@wesleyan.edu>
2024-03-05 23:22:26 -0500

    parser arguments corrected by --script

--- stacker.py
+++ stacker.py
@@ -3,7 +3,7 @@ import sys, os
 from file_manipulation import filter_traj_to_pdb, filter_traj
 from residue_movement import write_bottaro_to_csv
 from pairwise_distance import calculate_residue_distance, get_residue_distance_for_frame
-from visualization import display_arrays_as_video
+from visualization import display_arrays_as_video, visualize_two_residue_movement_heatmap, visualize_two_residue_movement_scatterplot
 
 class InvalidRoutine(Exception):
     pass
@@ -35,34 +35,99 @@ def run_python_command() -> None:
     Reads the command line input, runs the associated command with the
         added flags.
     '''
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(add_help=False, formatter_class=argparse.RawTextHelpFormatter)
+    global args;
+
+    args, remaining_args = parser.parse_known_args()
+
+    if '--help' in remaining_args or '-h' in remaining_args:
+        parser.add_argument("-s", "--script", metavar="ROUTINE", help='Name of command to use. OPTIONS:\n\n' + \
+                            "  filter_traj:\n\tfilters trajectory and topology files to desired residue numbers and atom names\n" + \
+                            "  bottaro:\n\tCreate polar plots like those in Figure 1 of Bottaro et. al (https://doi.org/10.1093/nar/gku972)\n" + \
+                            "  res_distance:\n\tGet the distance between two residues in a given frame\n" +\
+                            "  pairwise:\n\tCreate a stacking fingerprint of distances by residue", required=True, default='',
+                            choices=['filter_traj', 'bottaro', 'res_distance', 'pairwise'])
+        parser.add_argument("-h", "--help", help="show this help message and exit", action='help')
+        args = parser.parse_args()
+
+    parser.add_argument("-s", "--script", metavar="ROUTINE", help="Name of command to use (eg. filter_traj, bottaro, res_distance, pairwise)", required=True, default='')
+    
+    args, remaining_args = parser.parse_known_args()
+    print('getting here')
 
     # Organizes all possible arguments
     ## Determines which script/subroutine is to be run
-    parser.add_argument("-s", "--script", metavar="ROUTINE", help="Name of command to use (eg. filter_traj, bottaro, res_distance, pairwise)", required=True, default='')
-
     ## filter_traj requirements (--script filter_traj)
-    parser.add_argument("-trj", "--trajectory", metavar="TRAJECTORY_FILENAME", help="Filepath to trajectory file for the MD simulation", required=False)
-    parser.add_argument("-top", "--topology", metavar="TOPOLOGY_FILENAME", help="Filepath to Topology file for the MD simulation", required=False)
-    parser.add_argument("-o", "--output", metavar="OUTPUT_FILE", help="Filepath to output to, or prefix of output file if multiple outputs expected.", required=False)
-    parser.add_argument("-r", "--residues", metavar="RESIDUES", help="Comma-separated list of residues, also accepts colon (:) list creation (eg. 1:6 = 1,2,3,4,5)", required=False)
-    parser.add_argument("-a", "--atom_names", metavar="ATOM_NAMES", help="Comma-separated list of atom names", required=False, default="C2,C4,C6")
+    if args.script == 'filter_traj':
+        parser.add_argument("-trj", "--trajectory", metavar="TRAJECTORY_FILENAME", help="Filepath to trajectory file for the MD simulation", required=False)
+        parser.add_argument("-top", "--topology", metavar="TOPOLOGY_FILENAME", help="Filepath to Topology file for the MD simulation", required=False)
+        parser.add_argument("-o", "--output", metavar="OUTPUT_FILE", help="Filepath to output to, or prefix of output file if multiple outputs expected.", required=False)
+        parser.add_argument("-r", "--residues", metavar="RESIDUES", help="Smart-indexed list of 1-indexed residues, also accepts dash (-) list creation (eg. 1-6,10 = 1,2,3,4,5,10)", required=False, action = SmartIndexingAction)
+        parser.add_argument("-a", "--atom_names", metavar="ATOM_NAMES", help="Comma-separated list of atom names", required=False, default="C2,C4,C6")
 
     ## bottaro requirements (--script bottaro)
-    parser.add_argument("-pdb", "--pdb_input", metavar="PDB_INPUT", help="Filepath to input PDB file containing two residues, the perspective and viewed nucleotide", required=False)
-    parser.add_argument("-p", "--pers_res", metavar="PERSPECTIVE_RES", help="Comma-separated list of atomnames to use from residue 1 to find center of geometry for perspective nucleotide", required=False)
-    parser.add_argument("-v", "--view_res", metavar="VIEWED_RES", help="Comma-separated list of atomnames to use from residue 2 to find center of geometry for viewed nucleotide", required=False)
+    if args.script == 'bottaro':
+        parser.add_argument("-pdb", "--pdb_input", metavar="PDB_INPUT", help="Filepath to input PDB file containing two residues, the perspective and viewed nucleotide", required=False)
+        parser.add_argument("-p", "--pers_res", metavar="PERSPECTIVE_RES", help="residue index of the perspective residue whose plane to project onto. 0-/1-indexed based on -i flag (default: 1-indexed)", required=False)
+        parser.add_argument("-v", "--view_res", metavar="VIEWED_RES", help="residue index of the viewed residue whose midpoint will be projected onto perspective residue plane. 0-/1-indexed based on -i flag (default: 1-indexed)", required=False)
+        parser.add_argument("-pa", "--pers_atoms", metavar="PERSPECTIVE_ATOMS", help="Comma-separated list of atomnames to use from residue 1 to find center of geometry for perspective nucleotide", required=False)
+        parser.add_argument("-va", "--view_atoms", metavar="VIEWED_ATOMS", help="Comma-separated list of atomnames to use from residue 2 to find center of geometry for viewed nucleotide", required=False)
+        parser.add_argument("-i", "--index", metavar="INDEX", help="index (0-index or 1-index) for perspective/viewed residue numbers (default: 1-indexed)", required=False, default = 1)
+        parser.add_argument("-pt", "--plot_type", metavar="PLOT_TYPE", help="plot type (scatter or heat) to visualize Bottaro values. If empty string, then just write to csv with no visualization", required=False, default = '')
+        parser.add_argument("-po", "--plot_outfile", metavar="PLOT_OUTFILE", help="filename to output plot png to. If empty string, outputs to standard Python vis", required=False, default = '')
 
     ## res_distance requirements (--script res_distance)
-    parser.add_argument("-f", "--frame", type=int, metavar="FRAME_NUM", help="1-indexed Frame Number within trajectory to analyze", required=False)
+    if args.script == 'res_distance':
+        parser.add_argument("-f", "--frame", type=int, metavar="FRAME_NUM", help="1-indexed Frame Number within trajectory to analyze", required=False)
 
     ## pairwise requirements (--script pairwise)
-    parser.add_argument("-fl", "--frame_list", metavar="FRAME_LIST", help="Comma-separated list of 1-indexed Frame Numbers within trajectory to analyze", required=False)
-
-    global args;
-    args = parser.parse_args()
+    if args.script == 'pairwise':
+        parser.add_argument("-fl", "--frame_list", metavar="FRAME_LIST", help="Smart-indexed list of 1-indexed Frame Numbers within trajectory to analyze", required=False, action=SmartIndexingAction)
+    
+    if '--help' in remaining_args or '-h' in remaining_args:
+        parser.add_argument("-h", "--help", help="show this help message and exit", action='help')
+        args = parser.parse_args()
 
     convert_to_python_command()
+
+class SmartIndexingAction(argparse.Action):
+    '''
+    Custom argparse action to handle smart indexing of frame numbers.
+
+    Parses a comma-separated list of frame numbers with optional ranges (e.g., '1-20, 34, 25, 50-100')
+    and generates a list of individual frame numbers. Modifies the namespace by setting the attribute specified by the 'dest' parameter to the
+    list of individual frame numbers.
+
+    Args:
+        parser: argparse.ArgumentParser
+            The argparse parser object.
+        namespace: argparse.Namespace
+            The argparse namespace.
+        values: str
+            The input string containing frame numbers and ranges.
+        option_string: str, default=None
+            The option string.
+
+    Returns:
+        None
+
+    Examples:
+        >>> parser = argparse.ArgumentParser()
+        >>> parser.add_argument("-fl", "--frame_list", metavar="FRAME_LIST", help="Smart-indexed list of 1-indexed Frame Numbers within trajectory to analyze", required=False, action=SmartIndexingAction)
+        >>> args = parser.parse_args(["-fl", "1-20,34,25,50-100"])
+        >>> print(args.frame_list)
+        [1, 2, ..., 20, 34, 25, 50, 51, ..., 100]
+    '''
+    def __call__(self, parser, namespace, values, option_string=None):
+        frame_list = []
+        for item in values.split(','):
+            if '-' in item:
+                start, end = map(int, item.split('-'))
+                frame_list.extend(range(start, end + 1))
+            else:
+                frame_list.append(int(item))
+        frame_list.sort()
+        setattr(namespace, self.dest, frame_list)
 
 def convert_to_python_command() -> None:
     '''Converts a parsed command to use to the correct subroutine and runs the routine
@@ -112,21 +177,38 @@ def bottaro_routine() -> None:
         r, rho, and theta values for each frame of the PDB trajectory between the two residues.
         
     Example Usage:
-        [user]$ python3 stacker.py -s bottaro -pdb 5JUP_N2_tUAG_aCUA_+1GCU_nowat_mdcrd.pdb -o command_line_tests/bottaro/tUAG_aCUA_+1GCU_GC_plot.csv -p C2,C4,C6 -v C2,C4,C6
-        [user]$ python3 stacker.py -s bottaro -pdb 5JUP_N2_tUAG_aCUA_+1GCU_nowat_mdcrd_3200frames.pdb -o command_line_tests/bottaro/tUAG_aCUA_+1GCU_GC_plot_3200frames.csv -p C2,C4,C6 -v C2,C4,C6
+        [user]$ python3 stacker.py -s bottaro -pdb 5JUP_N2_tUAG_aCUA_+1GCU_nowat_mdcrd.pdb -o command_line_tests/bottaro/tUAG_aCUA_+1GCU_GC_plot.csv -pa C2,C4,C6 -va C2,C4,C6
+        [user]$ python3 stacker.py -s bottaro -pdb 5JUP_N2_tUAG_aCUA_+1GCU_nowat_mdcrd_3200frames.pdb -o command_line_tests/bottaro/tUAG_aCUA_+1GCU_GC_plot_3200frames.csv -pa C2,C4,C6 -va C2,C4,C6
     '''
-    if args.pers_res is not None:
-        perspective_atom_names = {res.strip() for res in args.pers_res.split(",")}
+
+    ##### check if PDB or MDTRAJ+TOP was passed in
+
+    if args.pers_atoms is not None:
+        perspective_atom_names = {res.strip() for res in args.pers_atoms.split(",")}
     else:
         AtomEmpty("Must include a list of atom names to define Pespective Residue center of geometry")
     
-    if args.view_res is not None:
-        viewed_atom_names = {res.strip() for res in args.view_res.split(",")}
+    if args.view_atoms is not None:
+        viewed_atom_names = {res.strip() for res in args.view_atoms.split(",")}
     else:
         AtomEmpty("Must include a list of atom names to define Viewed Residue center of geometry")
+    
+    if args.pers_res is not None:
+        pers_res_num = args.pers_res
+    else:
+        AtomEmpty("Must include a 1-indexed residue index for the perspective residue")
+    
+    if args.view_res is not None:
+        view_res_num = args.view_res
+    else:
+        AtomEmpty("Must include a 1-indexed residue index for the perspective residue")
 
     create_parent_directories(args.output)
-    write_bottaro_to_csv(pdb_filename=args.pdb_input, output_csv_name=args.output, res1_atom_names=perspective_atom_names, res2_atom_names=viewed_atom_names)
+    write_bottaro_to_csv(pdb_filename=args.pdb_input, output_csv_name=args.output, perspective_residue_num=pers_res_num, viewed_residue_num=view_res_num, res1_atom_names=perspective_atom_names, res2_atom_names=viewed_atom_names, index=args.index)
+    if args.plot_type == 'heat':
+        visualize_two_residue_movement_heatmap(args.output, plot_outfile=args.plot_outfile)
+    elif args.plot_type == 'scatter':
+        visualize_two_residue_movement_scatterplot(args.output, plot_outfile=args.plot_outfile)
 
 def res_distance_routine() -> None:
     '''Runs the Residue distance routine to determine the distance between the center of masses of two given residues
