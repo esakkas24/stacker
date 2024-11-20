09b29c4a0cc484e4ed24829e45fdbeeda5c4c12a
esakkas24 <esakkas@wesleyan.edu>
2024-11-20 15:39:11 -0500

    Added setup.py to install from pip

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
+# Function to read the requirements.txt file
+def read_requirements():
+    with open('requirements.txt') as req_file:
+        return req_file.read().splitlines()
+
 setup(
-    name="stacker",
+    name="StACKER",
     version="1.0.0",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'stacker=stacker.__init__:run_python_command'
+            'stacker=stacker.__init__:run_python_command', 
         ],
     },
+    install_requires=read_requirements(), 
 )
